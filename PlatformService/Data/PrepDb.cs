8c0152e43b82ca94345a659358b96a81e33281d7
Vininhos <viniciusrw2011@hotmail.com>
2023-02-21 00:09:32 -0300

    Criado Dtos e classe de DB em memória

---
+++ PlatformService/Data/PrepDb.cs
@@ -0,0 +1,31 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public static class PrepDb
+    {
+        public static void PrepPopulation(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+        }
+
+        private static void SeedData(AppDbContext context)
+        {
+            if (!context.Platforms.Any())
+            {
+                Console.WriteLine("Seeding data...");
+
+                context.Platforms.AddRange(
+                    new Platform() { Name = "Dot Net", Publisher = "Microsoft", Cost = "Free" },
+                    new Platform() { Name = "Docker", Publisher = "Docker Inc.", Cost = "Free" },
+                    new Platform() { Name = "Amazon Web Services", Publisher = "Amazon", Cost = "Demand" }
+                    );
+
+                context.SaveChanges();
+            }
+            else
+                Console.WriteLine("Already have data.");
+        }
+    }
+}
--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -5,6 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
+builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -24,5 +26,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+PrepDb.PrepPopulation(app);
 
 app.Run();

fd1494f8b576eaa3a1266807df39f0c47894ee18
Vininhos <viniciusrw2011@hotmail.com>
2023-02-21 23:51:21 -0300

    Ajustado o nome da controller de plataform

--- PlatformService/Controllers/PlatformsController.cs
+++ PlatformService/Controllers/PlatformsController.cs
@@ -7,12 +7,12 @@ namespace PlatformService.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class PlatformController : ControllerBase
+    public class PlatformsController : ControllerBase
     {
         private readonly IPlatformRepo _repository;
         private readonly IMapper _mapper;
 
-        public PlatformController(IPlatformRepo repository, IMapper mapper)
+        public PlatformsController(IPlatformRepo repository, IMapper mapper)
         {
             _repository = repository;
             _mapper = mapper;

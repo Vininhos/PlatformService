c85f12872daf6f3cc5d063be8c994fdfe2ccb6fe
Vininhos <viniciusrw2011@hotmail.com>
2023-02-26 18:45:13 -0300

    Criado serviço de plataforma no K8S, criado novo projeto CommandsService e adicionado comm. async entre ambos

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using PlatformService.Data;
+using PlatformService.SyncDataServices.Http;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
+builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
 
 builder.Services.AddControllers();
 
@@ -16,6 +18,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+Console.WriteLine($"Command Service Endpoint {app.Configuration["CommandService"]}");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
---
+++ PlatformService/SyncDataServices/Http/HttpCommandDataClient.cs
@@ -0,0 +1,30 @@
+﻿using PlatformService.Dtos;
+using System.Text;
+using System.Text.Json;
+
+namespace PlatformService.SyncDataServices.Http
+{
+    public class HttpCommandDataClient : ICommandDataClient
+    {
+        private readonly HttpClient _httpclient;
+        private readonly IConfiguration _configuration;
+
+        public HttpCommandDataClient(HttpClient httpClient, IConfiguration configuration)
+        {
+            _httpclient = httpClient;
+            _configuration = configuration;
+        }
+
+        public async Task SendPlatformToCommand(PlatformReadDto plat)
+        {
+            var httpContent = new StringContent(JsonSerializer.Serialize(plat), Encoding.UTF8, "application/json");
+
+            var response = await _httpclient.PostAsync($"{_configuration["CommandService"]}", httpContent);
+
+            if(response.IsSuccessStatusCode)
+                Console.WriteLine("Sync POST to CommandService was OK!");
+            else
+                Console.WriteLine("Sync POST to CommandService was NOT OK!");
+        }
+    }
+}

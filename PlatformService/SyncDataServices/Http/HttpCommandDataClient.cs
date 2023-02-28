77b0fac58bcc7fdec397dd63a00c83c5f07cceba
Vininhos <viniciusrw2011@hotmail.com>
2023-02-27 23:52:00 -0300

    Ajustado arquivos de deploy, appsettings.json e circundado o método SendPlatformToCommand com try catch

--- PlatformService/SyncDataServices/Http/HttpCommandDataClient.cs
+++ PlatformService/SyncDataServices/Http/HttpCommandDataClient.cs
@@ -17,14 +17,21 @@ namespace PlatformService.SyncDataServices.Http
 
         public async Task SendPlatformToCommand(PlatformReadDto plat)
         {
-            var httpContent = new StringContent(JsonSerializer.Serialize(plat), Encoding.UTF8, "application/json");
+            try
+            {
+                var httpContent = new StringContent(JsonSerializer.Serialize(plat), Encoding.UTF8, "application/json");
 
-            var response = await _httpclient.PostAsync($"{_configuration["CommandService"]}", httpContent);
+                var response = await _httpclient.PostAsync($"{_configuration["CommandService"]}", httpContent);
 
-            if(response.IsSuccessStatusCode)
-                Console.WriteLine("Sync POST to CommandService was OK!");
-            else
-                Console.WriteLine("Sync POST to CommandService was NOT OK!");
+                if (response.IsSuccessStatusCode)
+                    Console.WriteLine("Sync POST to CommandService was OK!");
+                else
+                    Console.WriteLine("Sync POST to CommandService was NOT OK!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
     }
 }
